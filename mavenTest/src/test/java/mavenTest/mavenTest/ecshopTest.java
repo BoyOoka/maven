39e5d234f61c1cd95b3ac9da13e2fc4f3938329d
gaya <gaya@192.168.1.120>
2017-09-10 14:55:22 +0800

    test

---
+++ mavenTest/src/test/java/mavenTest/mavenTest/ecshopTest.java
@@ -0,0 +1,106 @@
+package mavenTest.mavenTest;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ecshopTest{
+	Element e = new Element();
+	WebDriver driver;
+  @Test(priority=1)
+  public void login() {
+	  //登录
+	 driver.findElement(e.userLogin).click();
+	 driver.findElement(e.userName).sendKeys(e.user);
+	 driver.findElement(e.userPassword).sendKeys(e.password);
+	 driver.findElement(e.userSubmit).click();
+	 
+	 //验证正确登录
+	 assertEquals(driver.findElement(e.userInfoL).getText(), e.userInfo);
+  }
+  /*
+   *搜索商品加入购物车付款 
+   */
+  @Test(priority=2)
+  public void serch() throws InterruptedException {
+	  driver.findElement(e.serchText).sendKeys("恒基伟业G101");
+	  driver.findElement(e.serchButton).click();
+	  driver.findElement(e.good).click();
+	  driver.findElement(e.shopCarButton).click();;
+	  driver.findElement(e.payButton).click();;
+	  driver.findElement(e.payInputButton).click();
+  }
+  /*
+   * 管理员发货
+   */
+  @Test(priority=3)
+  public void delivery(){
+		driver.get(e.adminUrl);
+		driver.findElement(e.adminUserNmae).sendKeys(e.adminName);
+		driver.findElement(e.adminUserPassword).sendKeys(e.adminPassword);
+		driver.findElement(e.adminlogin).click();
+		//切换到headerframe
+		driver.switchTo().frame(driver.findElement(By.id("header-frame")));
+		driver.findElement(e.orderList).click();
+		driver.switchTo().parentFrame();
+		//切换到main-frame
+		driver.switchTo().frame("main-frame");
+		driver.findElement(e.orderListChose).click();
+		driver.findElement(e.shipButton).click();
+		driver.findElement(e.deliverConfir).click();
+		
+		
+	try{
+		System.out.println(1);
+		driver.findElement(e.toDeliver).click();
+	}catch(WebDriverException we){
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		System.out.println(2);
+		wait.until(ExpectedConditions.elementToBeClickable(e.toDeliver));
+		driver.findElement(e.toDeliver).click();
+	}
+		
+		driver.findElement(e.check).click();
+		driver.findElement(e.deliverConfirm).click();
+	  }
+  	/*
+  	 * 确认收货
+  	 */
+	@Test(priority=4)
+	public void confirmgoods(){
+		driver.get(e.url);
+		driver.findElement(e.userCenter).click();
+		driver.findElement(e.myGoodslist).click();
+		driver.findElement(e.confirmGoods).click();
+		driver.switchTo().alert().accept();
+	}
+  
+  @BeforeClass
+  public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver","/Users/gaya/git/mavenTest/mavenTest/chromedriver");
+		driver = new ChromeDriver();
+//		System.setProperty("webdriver.gecko.driver",path+"/lib/selenium/geckodriver");
+//		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(e.url);
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.close();
+	  driver.quit();
+  }
+
+}
